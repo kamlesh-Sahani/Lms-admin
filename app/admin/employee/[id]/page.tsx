7eb8ca8c55853c099883061b97e6db1e1dd53b0e
kamlesh_sahani <alex20150001969@gmail.com>
2024-08-01 15:30:27 +0530

    edit

--- app/admin/employee/[id]/page.tsx
+++ app/admin/employee/[id]/page.tsx
@@ -1,131 +1,294 @@
-import React from 'react'
+"use client";
 
-const editDetail = () => {
+import React, { useState } from "react";
+import { ImProfile } from "react-icons/im";
+import { BsPersonWorkspace } from "react-icons/bs";
+import { FaUserGraduate } from "react-icons/fa";
+
+const EditPage = () => {
+  const [edit, setEdit] = useState<boolean>(true);
+  const [editProfessional, setEditProfessional] = useState<boolean>(true);
+  const [editEducational, setEditEducational] = useState<boolean>(true);
   return (
-    <div className="flex flex-col items-center h-screen w-screen">
-      <div className="py-4">
-        <h1 className='text-2xl font-bold mb-4'>Edit Personal details</h1>
-        <div className=" w-[96]">
-          <div>
-     
-            <div className="bg-white rounded-lg shadow-md p-4 mt-4">
-              <div className="flex space-x-4">
-                <div className="w-1/2 pb-2">
-                  <label htmlFor="firstname">First Name</label>
-                  <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    placeholder="First name"
-                    type="text"
-                    name="firstname"
-                    id="firstname"
-                  />
-                </div>
-                <div className="w-1/2">
-                  <label htmlFor="lastname">Last Name</label>
-                  <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    placeholder="Last name"
-                    type="text"
-                    name="lastname"
-                    id="lastname"
-                  />
-                </div>
-              </div>
-              <label
-                htmlFor="email"
-                className="block my-2 text-gray-700 font-bold"
-              >
-                Email Address
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                placeholder="Email Address"
-              />
-            <div className="flex space-x-4 my-2">
-                <div className="w-1/3">
-                  <label htmlFor="firstname">First Name</label>
-                  <input
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    placeholder="First name"
-                    type="text"
-                    name="firstname"
-                    id="firstname"
-                  />
-                </div>
-                <div className="w-1/3">
-                  <input
-                    className="shadow appearance-none border rounded w-full mt-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    placeholder="Last name"
-                    type="text"
-                    name="lastname"
-                    id="lastname"
-                  />
-                </div>
-                <div className="w-1/3">
-                  <input
-                    className="shadow appearance-none border rounded w-full mt-6 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    placeholder="Last name"
-                    type="text"
-                    name="lastname"
-                    id="lastname"
-                  />
-                </div> 
-              </div>
+    <div className="flex flex-col w-full max-w-screen max-h-screen h-full  no-scrollbar">
+      <div className="p-5">
+        <h1 className="text-3xl mt-2 ml-5 font-black">Edit Profile</h1>
+        <p className="text-base ml-5 text-gray-500">
+          Here you make changes according to your prefrence
+        </p>
+      </div>
 
-              <label
-                htmlFor="city"
-                className="block my-2 text-gray-700 font-bold"
-              >
-                City
-              </label>
-              <input
-                type="text"
-                id="city"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                placeholder="Your city"
-              />
-              <label
-                htmlFor="streetAddress"
-                className="block my-2 text-gray-700 font-bold"
-              >
-                Street Address
-              </label>
-              <input
-                type="text"
-                id="streetAddress"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                placeholder="Your street address"
-              />
-              <label
-                htmlFor="state"
-                className="block my-2 text-gray-700 font-bold"
-              >
-                State/Province
-              </label>
-              <input
-                type="text"
-                id="state"
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                placeholder="Your state or province"
-              />
-            </div>
-
-            <div className="flex justify-end mt-4">
-              <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2">
-                Discard
-              </button>
-              <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                Save
-              </button>
-            </div>
+      <div className="  w-full h-full rounded-lg max-h-screen  overflow-scroll no-scrollbar">
+        <div className="ml-5 shadow-md dark:shadow-white border mb-3 rounded-lg mr-5 pb-4">
+          <div className="flex gap-2 py-2 bg-gray-200 rounded-t-lg">
+            <ImProfile className="mt-1 text-3xl ml-4" />
+            <h1 className="text-2xl mt-1">Personal Details</h1>
           </div>
+          {edit ? (
+            <div className="flex  max-sm:flex-col mt-5 px-5">
+              <div className="w-full flex flex-col gap-2">
+                <div className="flex w-full ">
+                  <p className="w-1/2 text-lg ">Name:</p>
+                  <p className="">Tahir Khan</p>
+                </div>
+                <div className="flex">
+                  <p className="w-1/2 text-lg ">DOB:</p>
+                  <p>17-05-2006</p>
+                </div>
+                <div className="flex">
+                  <p className="w-1/2 text-lg ">Gender:</p>
+                  <p>Male</p>
+                </div>
+                <div className="flex">
+                  <p className="w-1/2 text-lg ">Contact:</p>
+                  <p>9783468480</p>
+                </div>
+                <div className="flex">
+                  <p className="w-1/2 text-lg ">Address:</p>
+                  <p className="w-1/2">D-27, Shaheen Bagh Okhla</p>
+                </div>
+              </div>
+
+              <div className="sm:w-full max-sm:flex mt-2">
+                <button
+                  onClick={() => setEdit(!edit)}
+                  className="text-white max-sm:w-full max-sm:mx-1 bg-blue-500 p-2 rounded-sm "
+                >
+                  Edit Profile
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="flex max-lg:flex-col mt-5 mx-5 ">
+              <div className="w-full flex flex-col gap-2 h-full">
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Name:</label>
+                  <input
+                    type="text"
+                    className="p-2 lg:w-1/4 border dark:border-black  rounded-lg outline-none"
+                    placeholder="Tahir Khan"
+                  />
+                </div>
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">DOB:</label>
+                  <input
+                    type="date"
+                    className="p-2 lg:w-1/4 border dark:border-black  rounded-lg outline-none"
+                    placeholder="17-05-2006"
+                  />
+                </div>
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Gender:</label>
+                  <input
+                    type="text"
+                    className="p-2 lg:w-1/4 rounded-lg border dark:border-black  outline-none"
+                    placeholder="Male"
+                  />
+                </div>
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Contact:</label>
+                  <input
+                    type="phone"
+                    className="p-2 lg:w-1/4 rounded-lg border dark:border-black  outline-none"
+                    placeholder="9783468480"
+                  />
+                </div>
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Address:</label>
+                  <input
+                    type="text"
+                    className="p-2 lg:w-1/4 rounded-lg border dark:border-black  outline-none"
+                    placeholder="D-27, Shaheen Bagh Okhla"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:w-full max-sm:flex mt-2">
+                <button
+                  onClick={() => setEdit(!edit)}
+                  className="text-white max-sm:w-full max-sm:mt-2 bg-blue-500 p-2 rounded-sm "
+                >
+                  Save Changes
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Professional details */}
+
+        <div className="ml-5 shadow-md dark:shadow-white border mb-3 rounded-lg mr-5 pb-4">
+          <div className="flex gap-2 py-2 bg-gray-200 rounded-t-lg">
+            <BsPersonWorkspace className="mt-1 ml-4 text-3xl" />
+            <h1 className="text-2xl mt-1">Professional Details</h1>
+          </div>
+          {editProfessional ? (
+            <div className="flex  max-sm:flex-col mt-5 mx-5">
+              <div className="w-full flex flex-col gap-2">
+                <div className="flex w-full ">
+                  <p className="w-1/2 text-lg ">Department Id:</p>
+                  <p className="">dbit001</p>
+                </div>
+                <div className="flex">
+                  <p className="w-1/2 text-lg ">Designation:</p>
+                  <p>Software Engineer</p>
+                </div>
+                <div className="flex">
+                  <p className="w-1/2 text-lg ">Date of Joining:</p>
+                  <p>17-05-2020</p>
+                </div>
+                <div className="flex">
+                  <p className="w-1/2 text-lg ">Employement Type:</p>
+                  <p>Full-Time</p>
+                </div>
+              </div>
+
+              <div className="sm:w-full max-sm:flex mt-2">
+                <button
+                  onClick={() => setEditProfessional(!editProfessional)}
+                  className="text-white max-sm:w-full max-sm:mx-1 bg-blue-500 p-2 rounded-sm "
+                >
+                  Edit Profile
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="flex  max-lg:flex-col mt-3 mx-5">
+              <div className="w-full flex flex-col gap-2">
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Department Id:</label>
+                  <input
+                    type="text"
+                    className="p-2 lg:w-1/4 border dark:border-black  rounded-lg outline-none"
+                    placeholder="dbit001"
+                  />
+                </div>
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Designation:</label>
+                  <input
+                    type="text"
+                    className="p-2 lg:w-1/4 border dark:border-black  rounded-lg outline-none"
+                    placeholder="Software Engineer"
+                  />
+                </div>
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Date of Joining:</label>
+                  <input
+                    type="date"
+                    className="p-2 lg:w-1/4 rounded-lg border dark:border-black  outline-none"
+                    placeholder="17-05-2020"
+                  />
+                </div>
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Employement Type:</label>
+                  <input
+                    type="phone"
+                    className="p-2 lg:w-1/4 rounded-lg border dark:border-black  outline-none"
+                    placeholder="Full-Time"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:w-full max-sm:flex mt-2">
+                <button
+                  onClick={() => setEditProfessional(!editProfessional)}
+                  className="text-white max-sm:w-full max-sm:mt-2 bg-blue-500 p-2  rounded-sm "
+                >
+                  Save Changes
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Eduaction details */}
+
+        <div className="ml-5 shadow-md dark:shadow-white border mb-3 rounded-lg mr-5 pb-4">
+          <div className="flex gap-2 py-2 bg-gray-200 rounded-t-lg">
+            <FaUserGraduate className="mt-1 ml-4 text-3xl" />
+            <h1 className="text-2xl mt-1">Educational Details</h1>
+          </div>
+          {editEducational ? (
+            <div className="flex  max-sm:flex-col mt-5 mx-5">
+              <div className="w-full flex flex-col gap-2">
+                <div className="flex w-full ">
+                  <p className="w-1/2 text-lg ">Qualification:</p>
+                  <p className="">Graduate</p>
+                </div>
+                <div className="flex">
+                  <p className="w-1/2 text-lg ">University:</p>
+                  <p>ABC University</p>
+                </div>
+                <div className="flex">
+                  <p className="w-1/2 text-lg ">Year of Passing:</p>
+                  <p>17-05-2018</p>
+                </div>
+                <div className="flex">
+                  <p className="w-1/2 text-lg ">Specialization:</p>
+                  <p>Computer Science</p>
+                </div>
+              </div>
+
+              <div className="sm:w-full max-sm:flex mt-2">
+                <button
+                  onClick={() => setEditEducational(!editEducational)}
+                  className="text-white max-sm:w-full max-sm:mx-1 bg-blue-500 p-2 rounded-sm "
+                >
+                  Edit Profile
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="flex  max-lg:flex-col mt-3 mx-5">
+              <div className="w-full flex flex-col gap-2">
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Qualification:</label>
+                  <input
+                    type="text"
+                    className="p-2 lg:w-1/4 border dark:border-black  rounded-lg outline-none"
+                    placeholder="Graduate"
+                  />
+                </div>
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">University:</label>
+                  <input
+                    type="text"
+                    className="p-2 lg:w-1/4 border dark:border-black  rounded-lg outline-none"
+                    placeholder="Software Engineer"
+                  />
+                </div>
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Year of Passing:</label>
+                  <input
+                    type="date"
+                    className="p-2 lg:w-1/4 rounded-lg border dark:border-black  outline-none"
+                    placeholder="17-05-2018"
+                  />
+                </div>
+                <div className="flex max-lg:flex-col">
+                  <label className="w-1/2 text-lg ">Specialization:</label>
+                  <input
+                    type="phone"
+                    className="p-2 lg:w-1/4 rounded-lg border dark:border-black  outline-none"
+                    placeholder="Computer Science"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:w-full max-sm:flex mt-2">
+                <button
+                  onClick={() => setEditEducational(!editEducational)}
+                  className="text-white max-sm:w-full  max-sm:mt-2 bg-blue-500 p-2 rounded-sm "
+                >
+                  Save Changes
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
-    
   );
-}
+};
 
-export default editDetail
+export default EditPage;
--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,11 @@
 import { ReactNode } from "react";
-
+import Sidebar from "@/components/Sidebar";
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <>{children}</>;
+  return (
+    <div className="flex w-full h-screen">
+      <main className="max-w-6xl flex-1 mx-auto py-4">{children}</main>
+    </div>
+  );
 };
 
 export default Layout;
